a5675b746175d40f64e0b86578cd20f23b43c4d5
QuyLV <levanquy15052001@gmail.com>
2023-10-21 23:07:29 +0700

    tam

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,11 @@
 import  express  from "express";
+import HomeController from "../controllers/HomeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("hello world");
-  });
+  router.get("/",HomeController.getHomePage);
+
   return app.use("/", router);
 };
 
