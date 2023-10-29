1473af0023f6c0ba7fde1e6a5aeb33cdb87170cf
QuyLV <levanquy15052001@gmail.com>
2023-10-29 16:57:35 +0700

    done lesson 21

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWebRoutes from './route/web';
-require('dotenv').config();
+import initWebRoutes from "./route/web";
+import connectDB from "./config/conncectDB";
+
+require("dotenv").config();
 
 let app = express();
 
@@ -13,9 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
-    //callback
-    console.log("back end nodejs is runing on the port:"+port);
+  //callback
+  console.log("back end nodejs is runing on the port:" + port);
 });
